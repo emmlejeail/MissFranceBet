da6c62cab65609ceba8a2ae18b57dda76aadbf01
Emmanuelle Lejeail <emmanuelle.lejeail@amadeus.com>
2020-11-15 22:04:00 +0100

    Add rooting to admin when asked in url

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CrudService } from "../shared/crud.service";
 
 @Component({
   selector: 'app-admin',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  constructor(private crudService: CrudService) { }
 
   ngOnInit(): void {
   }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
+import { MissComponent } from './miss/miss.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MissComponent,},
+  {path: 'admin', component: AdminComponent,}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
